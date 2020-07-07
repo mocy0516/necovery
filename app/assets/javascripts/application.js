67e0acc27e39927239d04be747f4034562cb6f66
今井源希 <mocky0516@mokibook-pro.local>
2020-07-07 12:18:05 +0900

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,9 @@
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require_tree .
+$(function() {
+
+    setTimeout("$('.flash').fadeOut(1500)",1500)
+
+});
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      image_name: "default_user.jpg"
+    )
+    if @user.save
+      flash[:notice] = "飼い主になったニャ！"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new")
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+
+    if params[:image]
+      @user.image_name="#{@user.id}.jpg"
+      image=params[:image]
+      File.binwrite("/Users/mocky0516/Desktop/necovery/public/user_images/#{@user.image_name}",image.read)
+    end
+
+    if @user.save
+      flash[:notice] = "やりなおしたニャ！"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/edit")
+    end
+  end
+end
