521d83d227ff0985b4d76473258363b0ce042c48
今井源希 <mocky0516@mokibook-pro.local>
2020-07-07 13:53:13 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-
+  resources :necovery
+  root "home#top"
+  
   post "users/:id/update" => "users#update"
   get "users/:id/edit" => "users#edit"
   post "users/create" => "users#create"
