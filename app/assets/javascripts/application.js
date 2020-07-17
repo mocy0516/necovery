0e3046b36bd4aa81c84ec240940afeb6ec41a55b
今井源希 <mocky0516@mokibook-pro.local>
2020-07-17 15:57:30 +0900

    20200717

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,35 +2,38 @@
 //= require_tree .
 //= require jquery
 -//= require jquery_ujs
-$(function() {
-  setTimeout(function(){
-    $('.flash').slideUp(500);
-  },1000);
+document.addEventListener("turbolinks:load"
+, function () {
+  $(function() {
+    setTimeout(function(){
+      $('.flash').slideUp(500);
+    },1000);
 
-  $('#postUp').change(function(e){
-    //ファイルオブジェクトを取得する
-    var file = e.target.files[0];
-    var reader = new FileReader();
-    var $preview = $("#preview");
+    $('#postUp').change(function(e){
+      //ファイル取得
+      var file = e.target.files[0];
+      var reader = new FileReader();
+      var $preview = $("#preview");
 
-    //画像でない場合は処理終了
-    if(file.type.indexOf("image") < 0){
-      alert("画像ファイルを指定してください。");
+      //画像でない場合は処理終了
+      if(file.type.indexOf("image") < 0){
+        alert("画像ファイルを指定してください。");
       return false;
-    }
+      }
 
-    //アップロードした画像を設定する
-    reader.onload = (function(file){
-      return function(e){
-        $("label").hide();
-        $(".after-select-img").show();
-        $preview.append($('<img>').attr({
-          src: e.target.result,
-          title: file.name
-        }));
-      };
-    })(file);
+      //アップロードした画像を設定
+      reader.onload = (function(file){
+        return function(e){
+          $("label").hide();
+          $(".after-select-img").show();
+          $preview.append($('<img>').attr({
+            src: e.target.result,
+            title: file.name
+          }));
+        };
+      })(file);
 
-    reader.readAsDataURL(file);
+      reader.readAsDataURL(file);
+    });
   });
 });
